62c9a14c4f97591b6a08ecc38f05479dcd1e8780
Robert Miller <robert.miller@door3.com>
2020-09-11 15:46:21 -0400

    add FileHelper and ReadCsv, FindFile functions
    Signed-off-by: Robert Miller <robert.miller@door3.com>

---
+++ app/src/D3SK.NetCore.Common/Models/ReadCsvOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using CsvHelper;
+using D3SK.NetCore.Common.Extensions;
+
+namespace D3SK.NetCore.Common.Models
+{
+    public class ReadCsvOptions
+    {
+        public Encoding Encoding { get; set; } = Encoding.Default;
+
+        public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
+
+        public Action<CsvReader> BeforeParsingAction { get; set; }
+
+        public Action<IList<string>> AfterParsingAction { get; set; }
+
+        public Func<CsvHelperException, bool> OnReadingException { get; set; }
+
+        public Action<bool, string[], int, ReadingContext> OnHeaderValidated { get; set; }
+
+        public Func<ReadingContext, bool> BadDataFunc { get; set; }
+
+        public ReadCsvOptions()
+        {
+        }
+
+        public ReadCsvOptions(Encoding encoding)
+        {
+            Encoding = encoding.NotNull(nameof(encoding));
+        }
+    }
+}
---
+++ app/src/D3SK.NetCore.Common/Utilities/FileHelper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using D3SK.NetCore.Common.Extensions;
+using D3SK.NetCore.Common.Models;
+
+namespace D3SK.NetCore.Common.Utilities
+{
+    public static class FileHelper
+    {
+        public static IList<T> ReadCsv<T>(string filePath, ReadCsvOptions options = null)
+        {
+            options ??= new ReadCsvOptions();
+
+            using var reader = new StreamReader(filePath, options.Encoding);
+            using var csv = new CsvReader(reader, options.CultureInfo, false);
+
+            options.BeforeParsingAction?.Invoke(csv);
+            var badDataFunc = options.BadDataFunc ?? (context => true);
+
+            var good = new List<T>();
+            var bad = new List<string>();
+            var isRecordBad = false;
+
+            csv.Configuration.BadDataFound = context =>
+            {
+                if (badDataFunc(context))
+                {
+                    isRecordBad = true;
+                    bad.Add(context.RawRecord);
+                }
+            };
+
+            csv.Configuration.ReadingExceptionOccurred = options.OnReadingException ?? (ex => false);
+            csv.Configuration.HeaderValidated =
+                options.OnHeaderValidated ?? ((isValid, headerNames, headerNameIndex, context) => { });
+
+            while (csv.Read())
+            {
+                var record = csv.GetRecord<T>();
+                if (!isRecordBad)
+                {
+                    good.Add(record);
+                }
+
+                isRecordBad = false;
+            }
+
+            options.AfterParsingAction?.Invoke(bad);
+            return good.ToList();
+        }
+
+        public static IList<T> ReadCsv<T>(string filePath, Encoding encoding)
+        {
+            return ReadCsv<T>(filePath, new ReadCsvOptions(encoding));
+        }
+
+        public static IList<T> ReadCsv<T, TMap>(string filePath, ReadCsvOptions options = null)
+            where TMap : ClassMap<T>
+        {
+            options ??= new ReadCsvOptions();
+            var bpAction = options.BeforeParsingAction;
+            options.BeforeParsingAction = csv =>
+            {
+                csv.Configuration.RegisterClassMap<TMap>();
+                bpAction?.Invoke(csv);
+            };
+            return ReadCsv<T>(filePath, options);
+        }
+
+        public static IList<T> ReadCsv<T, TMap>(string filePath, Encoding encoding)
+            where TMap : ClassMap<T>
+        {
+            return ReadCsv<T, TMap>(filePath, new ReadCsvOptions(encoding));
+        }
+
+        public static bool FileExists(string path, string fileName, string relativePath = null)
+        {
+            return FindFile(path, fileName, relativePath) != null;
+        }
+
+        public static FileInfo FindFile(string path, string fileName, string relativePath = null)
+        {
+            while (true)
+            {
+                var filePath = Path.Combine(path, relativePath, fileName);
+                if (File.Exists(filePath))
+                {
+                    return new FileInfo(filePath);
+                }
+
+                var newPath = Directory.GetParent(path)?.FullName;
+                if (newPath.IsEmpty())
+                {
+                    return null;
+                }
+
+                path = newPath;
+            }
+        }
+
+        public static string GenerateUniqueFileName(string fileName, string path)
+        {
+            var originalName = Path.GetFileNameWithoutExtension(fileName);
+            var ext = Path.GetExtension(fileName);
+            var nameCounter = 1;
+
+            while (File.Exists(Path.Combine(path, fileName)))
+            {
+                fileName = $"{originalName}{'_'}{nameCounter++}{ext}";
+            }
+
+            return fileName;
+        }
+
+        public static async Task<string> SaveFileAsync(string path, string fileName, Stream stream)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            fileName = GenerateUniqueFileName(fileName, path);
+            var filePath = Path.Combine(path, fileName);
+
+            await using (var file = new FileStream(filePath, FileMode.Create))
+            {
+                await stream.CopyToAsync(file);
+            }
+
+            return fileName;
+        }
+
+        public static string GetFileContentType(string filename)
+        {
+            var ext = Path.GetExtension(filename);
+            return MimeTypes.GetMimeType(ext);
+        }
+    }
+}
