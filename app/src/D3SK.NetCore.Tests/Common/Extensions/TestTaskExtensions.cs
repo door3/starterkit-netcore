9baea0752e9e9e679536127883de22ce56f42d8c
Robert Miller <robert.miller@door3.com>
2020-09-13 23:32:56 -0400

    add TestTaskExtensions WithAction test
    Signed-off-by: Robert Miller <robert.miller@door3.com>

---
+++ app/src/D3SK.NetCore.Tests/Common/Extensions/TestTaskExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using D3SK.NetCore.Common.Extensions;
+using Xunit;
+
+namespace D3SK.NetCore.Tests.Common.Extensions
+{
+    public class TestTaskExtensions
+    {
+        [Fact]
+        public async Task WithActionTest()
+        {
+            // test with Task.CompletedTask
+            var testActionInt1 = 0;
+            await Task.CompletedTask.WithAction(() => testActionInt1 = 17);
+            Assert.Equal(17, testActionInt1);
+
+            // test with another task, make sure action runs first
+            var testActionInt2 = 0;
+            async Task TestDelayTask(CancellationToken cancellationToken)
+            {
+                try
+                {
+                    // delay forever, must cancel token to continue
+                    await Task.Delay(int.MaxValue, cancellationToken);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+                finally
+                {
+                    testActionInt2 = 39;
+                }
+            }
+
+            using var testTaskCancellationTokenSource = new CancellationTokenSource();
+            var testTaskCancellationToken = testTaskCancellationTokenSource.Token;
+            var testTaskResult = TestDelayTask(testTaskCancellationToken).WithAction(() => testActionInt2 = 19);
+            Assert.Equal(19, testActionInt2);
+            // cancel token to break delay
+            testTaskCancellationTokenSource.Cancel();
+            // make sure rest of task completes
+            await testTaskResult;
+            Assert.Equal(39, testActionInt2);
+        }
+    }
+}
